8d5a6a912ce2a8ff06ccf176c259de4bb0629e90
Lee Nguyen <hvtransco@gmail.com>
2015-10-16 10:29:40 +0700

    communication - active tab

--- src/panels/debtorcommunications/Controller.js
+++ src/panels/debtorcommunications/Controller.js
@@ -1,35 +1,20 @@
 (function () {
 
-    angular.module('CmMerchantConfigApp')
-        .controller('DebtorCommunicationsController', Controller);
+  angular.module('CmMerchantConfigApp')
+  .controller('DebtorCommunicationsController', function($scope, $cookies, merchantConfigService) {
+    $scope.merchantConfigService = merchantConfigService;
+    merchantConfigService.debtorCommunicationsController = this;
 
-    function Controller($scope, merchantConfigService) {
-
-        $scope.merchantConfigService = merchantConfigService;
-        merchantConfigService.debtorCommunicationsController = this;
-        this.validate = validate;
-        $scope.validate = validate;
-        $scope.onSeriousArrearsNotificationsChange = onSeriousArrearsNotificationsChange;
-
-        var savedSeriousArrearsNotifications = null;
-
-        function validate() {
-            return $scope.form.validate(true);
-        }
-
-        function onSeriousArrearsNotificationsChange() {
-            if (merchantConfigService.seriousArrearsNotificationsEnabled) {
-                if (savedSeriousArrearsNotifications) {
-                    merchantConfigService.merchantConfig.debtorCommunications.seriousArrearsNotifications = savedSeriousArrearsNotifications;
-                }
-            } else {
-                savedSeriousArrearsNotifications = merchantConfigService.merchantConfig.debtorCommunications.seriousArrearsNotifications
-                    ? merchantConfigService.merchantConfig.debtorCommunications.seriousArrearsNotifications
-                    : null;
-                merchantConfigService.merchantConfig.debtorCommunications.seriousArrearsNotifications = null;
-            }
-        }
-
-    }
-
+    //communication active tab
+    $scope.communicationActiveTab = $cookies.communicationActiveTab || 'new';
+    /**
+     * active tab nav
+     * @param {String} tab
+     * @returns {void}
+     */
+    $scope.setActiveTab = function (tab) {
+      $cookies.communicationActiveTab = tab;
+      $scope.communicationActiveTab = tab;
+    };
+  });
 })();
